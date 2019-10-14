15cf88a21824badac9010bb3ebaaf23b782be78b
Kronykus <MoreheadSteve@gmail.com>
2019-10-14 18:12:04 -0500

    moved code for DRY stretch & styling tweaks

--- client/src/Movies/MovieList.jsx
+++ client/src/Movies/MovieList.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 import '../index.css';
 
 const MovieList = props => {
@@ -24,32 +26,10 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <Link className="link" to={`/movies/${movie.id}`}>
-          <MovieDetails key={movie.id} movie={movie} />
+        <Link className="link" key={movie.id} to={`/movies/${movie.id}`}>
+          <MovieCard key={movie.id} movie={movie} type='list' />
         </Link>
       ))}
-    </div>
-  );
-}
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
     </div>
   );
 }
